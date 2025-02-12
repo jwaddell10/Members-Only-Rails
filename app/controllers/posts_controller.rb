7bf33794c64f12dafa188a8aa223ca404f91f4c4
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2025-02-12 09:14:46 -0600

    created form that lets users submit posts, able to display post on screen

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,27 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to "/posts"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def post_params
+      params.expect(post: [ :text ])
+    end
 end
